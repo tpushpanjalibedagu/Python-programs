aca65852c9076e5baac8f9557ae250083677838b
T Pushpanjali Beadgu <97530896+thearcaneabstract@users.noreply.github.com>
2022-05-25 16:39:41 +0530

    Create Frequency.py
    function that takes a string and prints the letters in decreasing order of frequency

---
+++ Frequency.py
@@ -0,0 +1,14 @@
+def most_frequent(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+
+def sorted_value(d,reverse = True):
+    return dict(sorted(d.items(), key = lambda x: x[1], reverse = reverse))
+string = input("Please enter the string")
+print(sorted_value(most_frequent(string)))
